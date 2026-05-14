18d9df2a69e89e2727388208eab0ca49eb2864d0
pgi26 <techinerta.pgi@gmail.com>
2026-05-14 18:48:37 +0530

    first commit

---
+++ backend/server.py
@@ -0,0 +1,81 @@
+import os
+import psycopg2
+
+from typing import List
+from pydantic import BaseModel
+
+import uvicorn
+from fastapi import FastAPI, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+UPLOAD_FOLDER = "uploads"
+
+class VideoModel(BaseModel):
+    id: int
+    video_title: str
+    video_url: str
+
+
+app = FastAPI(debug=True)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.mount("/uploads", StaticFiles(directory=UPLOAD_FOLDER), name="uploads")
+
+
+@app.get("/status")
+async def check_status():
+    return "Hello World!"
+
+
+@app.get("/videos", response_model=List[VideoModel])
+async def get_videos():
+    conn = psycopg2.connect(
+        database="exampledb", user="docker", password="docker", host="localhost"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM video ORDER BY id DESC")
+    rows = cur.fetchall()
+
+    formatted_videos = []
+    for row in rows:
+        formatted_videos.append(
+            VideoModel(id=row[0], video_title=row[1], video_url=row[2])
+        )
+
+    cur.close()
+    conn.close()
+    return formatted_videos
+
+
+@app.post("/videos", status_code=201)
+async def add_video(file: UploadFile):
+    # Save file locally
+    file_location = f"{UPLOAD_FOLDER}/{file.filename}"
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+
+    uploaded_file_url = f"http://127.0.0.1:8000/uploads/{file.filename}"
+
+    # Store URL in database
+    conn = psycopg2.connect(
+        database="exampledb", user="docker", password="docker", host="localhost"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        f"INSERT INTO video (video_title, video_url) VALUES ('{file.filename}', '{uploaded_file_url}')"
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
